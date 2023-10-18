cd9081487912ad2184bed9591639a971d734fc96
Simjust1 <simjust@mail.ru>
2023-10-18 10:56:02 +0500

    feat: add conditions for different numbers

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+  let result = [];
+
+  for (let y = 0; y < matrix.length; y++) {
+    for (let g = 0; g < matrix[y].length; g++) {
+      let rowCheck = y % 2;
+      if (rowCheck === 0) {
+        result.push(matrix[y][g]);
+      } else {
+        result.push(matrix[y][matrix.length-g-1]);
+      }
+    }
+  } return result;
+
 }
