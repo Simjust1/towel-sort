1b1e5a7d102f5ad9c666ec328bc175f2cae8070e
Simjust1 <simjust@mail.ru>
2023-10-20 16:06:41 +0500

    feat: add the working code after testing

--- src/index.js
+++ src/index.js
@@ -5,15 +5,29 @@ module.exports = function towelSort (matrix) {
 
   let result = [];
 
-  for (let y = 0; y < matrix.length; y++) {
-    for (let g = 0; g < matrix[y].length; g++) {
-      let rowCheck = y % 2;
-      if (rowCheck === 0) {
-        result.push(matrix[y][g]);
-      } else {
-        result.push(matrix[y][matrix.length-g-1]);
+  if (matrix === undefined) {
+    return result;
+  }
+  else {
+    for (let i = 0; i < matrix.length; i++) {
+      for (let y = 0; y < matrix[i].length; y++) {
+        let rowCheck = i % 2;
+        if (rowCheck === 0) {
+              result.push(matrix[i][y]);
+            }
+        else {
+          if(matrix.length === matrix[i].length) {
+            result.push(matrix[i][matrix.length - y - 1]);
+            console.log(matrix[i].length + '1');
+          }
+          else {
+            result.push(matrix[i][matrix.length - y]);
+              console.log(matrix[i].length + '2');
+          }
+        }
       }
-    }
-  } return result;
-
+    } return result;
+  }
 }
+
+
